779079de3823af98020c24f7b5b605ef1a5d2f18
gipark <gipark181@gmail.com>
2025-05-17 02:07:50 +0900

    [CHORE](event) Swagger 설정 추가 및 /swagger 엔드포인트 구성

--- event/src/main.ts
+++ event/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {INestApplication} from "@nestjs/common";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  addSwagger(app);
+
   await app.listen(process.env.PORT ?? 3002);
 }
+
+function addSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+      .setTitle('Event API')
+      .setDescription('이벤트 생성 / 참여 / 이벤트 관련 API')
+      .setVersion('0.1')
+      .addBearerAuth()
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+}
+
 bootstrap();
