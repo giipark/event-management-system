4b691a3d8073071184d2f32bac2cfee2430368fa
gipark <gipark181@gmail.com>
2025-05-17 02:07:36 +0900

    [CHORE](gateway) Swagger 설정 추가 및 /swagger 엔드포인트 구성

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {INestApplication} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  addSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
+function addSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+      .setTitle('Gateway API')
+      .setDescription('모든 API요청 라우팅 / 인증 / 권한 검사')
+      .setVersion('0.1')
+      .addBearerAuth()
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+}
+
 bootstrap();
