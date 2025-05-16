21fda87de812c24adacd09a2c336e76a8815f817
gipark <gipark181@gmail.com>
2025-05-17 02:07:44 +0900

    [CHORE](auth) Swagger 설정 추가 및 /swagger 엔드포인트 구성

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {INestApplication} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  addSwagger(app);
+
   await app.listen(process.env.PORT ?? 3001);
 }
+
+function addSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+      .setTitle('Auth API')
+      .setDescription('JWT 관리 / 회원가입(유저 등록) / 로그인 관련 API')
+      .setVersion('0.1')
+      .addBearerAuth()
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+}
+
 bootstrap();
