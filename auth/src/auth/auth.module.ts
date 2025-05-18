e366cf0cc529c0fa3254b08fdc22b76fcbc86453
gipark <gipark181@gmail.com>
2025-05-18 18:42:39 +0900

    [FEAT](auth) User 스키마 생성

--- auth/src/auth/auth.module.ts
+++ auth/src/auth/auth.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import {Module} from '@nestjs/common';
+import {AuthController} from './auth.controller';
+import {AuthService} from './auth.service';
+import {MongooseModule} from "@nestjs/mongoose";
+import {User, UserSchema} from "./schema/user.schema";
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+    imports: [
+        MongooseModule.forFeature([{name: User.name, schema: UserSchema}])
+    ],
+    controllers: [AuthController],
+    providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule {
+}
---
+++ auth/src/auth/schema/user.schema.ts
@@ -0,0 +1,24 @@
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import {Document} from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({timestamps: true})
+export class User {
+    @Prop({required: true, unique: true})
+    email: string;
+
+    @Prop({required: true})
+    password: string;
+
+    @Prop({default: 'USER', enum: ['USER', 'ADMIN']})
+    role: 'USER' | 'ADMIN';
+
+    @Prop()
+    nickname: string;
+
+    @Prop({unique: true})
+    recommendCode: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
