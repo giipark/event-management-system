8f7a8ba263c15b569dbb36d085ef2b2e92e1817d
gipark <gipark181@gmail.com>
2025-05-20 08:50:42 +0900

    [REFACTOR](auth) user 스키마 위치/구조 변경

--- auth/src/auth/auth.service.ts
+++ auth/src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import {ConflictException, Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
-import {User, UserDocument} from "./schema/user.schema";
+import {User, UserDocument} from "../user/schema/user.schema";
 import {InjectModel} from "@nestjs/mongoose";
 import {Model} from "mongoose";
 import {SignupDto} from "./dto/signup.dto";
