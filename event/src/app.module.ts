d28068aa427a5c1c56f7df25b501b2c85c5a1ff6
gipark <gipark181@gmail.com>
2025-05-20 05:21:44 +0900

    [CHORE](event) MongooseModule 의존성 주입 추가

--- event/src/app.module.ts
+++ event/src/app.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
-import { EventModule } from './event/event.module';
+import {Module} from '@nestjs/common';
+import {EventModule} from './event/event.module';
+import {MongooseModule} from "@nestjs/mongoose";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [EventModule],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        MongooseModule.forRoot(process.env.MONGO_DB),
+        EventModule
+    ],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
--- event/src/event/event.module.ts
+++ event/src/event/event.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { EventController } from './event.controller';
-import { EventService } from './event.service';
+import {Module} from '@nestjs/common';
+import {EventController} from './event.controller';
+import {EventService} from './event.service';
+import {MongooseModule} from "@nestjs/mongoose";
+import {Event, EventSchema} from "./schema/event.schema";
+import {EventBenefit, EventBenefitSchema} from "./schema/event-bnef.schema";
 
 @Module({
-  controllers: [EventController],
-  providers: [EventService]
+    imports: [
+        MongooseModule.forFeature([
+            {name: Event.name, schema: EventSchema},
+            {name: EventBenefit.name, schema: EventBenefitSchema},
+        ]),
+    ],
+    controllers: [EventController],
+    providers: [EventService]
 })
-export class EventModule {}
+export class EventModule {
+}
