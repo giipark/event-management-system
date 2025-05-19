00c0658d80bbe89e36d71c9cfd9ef90693d0e557
gipark <gipark181@gmail.com>
2025-05-19 20:41:38 +0900

    [DOCS](auth) User 스키마 필드별 설명 주석 추가

--- auth/src/auth/schema/user.schema.ts
+++ auth/src/auth/schema/user.schema.ts
@@ -5,18 +5,23 @@ export type UserDocument = User & Document;
 
 @Schema({timestamps: true})
 export class User {
+    /** 유저 이메일 */
     @Prop({required: true, unique: true})
     email: string;
 
+    /** 비밀번호 */
     @Prop({required: true})
     password: string;
 
+    /** 유저 권한 */
     @Prop({default: 'USER', enum: ['USER', 'ADMIN']})
     role: 'USER' | 'ADMIN';
 
+    /** 유저 닉네임 */
     @Prop({required: true})
     nickname: string;
 
+    /** 추천인코드 */
     @Prop({unique: true})
     recommendCode: string;
 }
