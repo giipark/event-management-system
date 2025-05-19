380154419fa9892f1dd8efdeb5b5e969e3cf1359
gipark <gipark181@gmail.com>
2025-05-20 08:41:49 +0900

    [CHORE](event) event.module에 EventRequest, EventWinner 스키마 의존성 주입 추가

--- event/src/event/event.module.ts
+++ event/src/event/event.module.ts
@@ -4,12 +4,16 @@ import {EventService} from './event.service';
 import {MongooseModule} from "@nestjs/mongoose";
 import {Event, EventSchema} from "./schema/event.schema";
 import {EventBenefit, EventBenefitSchema} from "./schema/event-bnef.schema";
+import {EventRequest, EventRequestSchema} from "./schema/event-req.schema";
+import {EventWinner, EventWinnerSchema} from "./schema/event-winner.schema";
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             {name: Event.name, schema: EventSchema},
             {name: EventBenefit.name, schema: EventBenefitSchema},
+            {name: EventRequest.name, schema: EventRequestSchema},
+            {name: EventWinner.name, schema: EventWinnerSchema},
         ]),
     ],
     controllers: [EventController],
