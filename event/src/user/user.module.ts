015a8558e0b56230445526e97c9febc6f0e0fc95
gipark <gipark181@gmail.com>
2025-05-20 13:24:13 +0900

    [CHORE](event) user 모듈에 EventWinner 스키마 의존성 주입 추가

--- event/src/user/user.module.ts
+++ event/src/user/user.module.ts
@@ -5,13 +5,15 @@ import {MongooseModule} from "@nestjs/mongoose";
 import {Event, EventSchema} from "../event/schema/event.schema";
 import {EventRequest, EventRequestSchema} from "../event/schema/event-req.schema";
 import {Inventory, InventorySchema} from "./schema/inventory.schema";
+import {EventWinner, EventWinnerSchema} from "../event/schema/event-winner.schema";
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             {name: Inventory.name, schema: InventorySchema},
-            {name: EventRequest.name, schema: EventRequestSchema},
             {name: Event.name, schema: EventSchema},
+            {name: EventRequest.name, schema: EventRequestSchema},
+            {name: EventWinner.name, schema: EventWinnerSchema},
         ]),
     ],
     controllers: [UserController],
