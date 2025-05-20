e144f2a49d76be8985d38aeea6089433d281ee1c
gipark <gipark181@gmail.com>
2025-05-20 18:54:40 +0900

    [FEAT](event) express 전역 Request type 추가

---
+++ event/src/types/request.d.ts
@@ -0,0 +1,18 @@
+import {RoleType} from "../user/schema/const/role-type.enum";
+import {Types} from "mongoose";
+
+declare global {
+    namespace Express {
+        interface User {
+            _id: Types.ObjectId;
+            email: string;
+            role: RoleType;
+        }
+
+        interface Request {
+            user: User;
+        }
+    }
+}
+
+export {};
